48751a4ef085eb1120bc500e8c168fc76e8e446b
Andrii Lashchenko <andrew.lashchenko@gmail.com>
2019-07-17 01:12:32 +0300

    Add TypesafeConfigOps.ConfigOptOps

---
+++ src/main/scala/io/github/typesafeconfigops/TypesafeConfigOps.scala
@@ -0,0 +1,24 @@
+package io.github.typesafeconfigops
+
+import com.typesafe.config.Config
+
+import scala.collection.JavaConverters._
+
+object TypesafeConfigOps {
+
+  implicit class ConfigOptOps(c: Config) {
+    def optConfig(path: String): Option[Config] = if (c.hasPath(path)) Option(c.getConfig(path)) else None
+    def optString(path: String): Option[String] = if (c.hasPath(path)) Option(c.getString(path)) else None
+    def optDouble(path: String): Option[Double] = if (c.hasPath(path)) Option(c.getDouble(path)) else None
+    def optInt(path: String): Option[Int] = if (c.hasPath(path)) Option(c.getInt(path)) else None
+    def optLong(path: String): Option[Long] = if (c.hasPath(path)) Option(c.getLong(path)) else None
+    def optBool(path: String): Option[Boolean] = if (c.hasPath(path)) Option(c.getBoolean(path)) else None
+    def optBigDecimal(path: String): Option[BigDecimal] = if (c.hasPath(path)) Option(BigDecimal(c.getString(path))) else None
+
+    def optIntList(path: String): Option[List[Int]] = if (c.hasPath(path)) Option(c.getIntList(path).asScala.map(_.toInt).toList) else None
+    def optLongList(path: String): Option[List[Long]] = if (c.hasPath(path)) Option(c.getLongList(path).asScala.map(_.toLong).toList) else None
+    def optStringList(path: String): Option[List[String]] = if (c.hasPath(path)) Option(c.getStringList(path).asScala.toList) else None
+    def optBigDecimalList(path: String): Option[List[BigDecimal]] = if (c.hasPath(path)) Option(c.getStringList(path).asScala.map(x => BigDecimal(x)).toList) else None
+  }
+
+}
